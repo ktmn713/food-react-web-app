3e28c2e627010cb68510d2f3b8ef9d65b8238cc5
ktmn713 <116754776+ktmn713@users.noreply.github.com>
2023-12-10 19:26:53 -0500

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import {HashRouter} from 'react-router-dom';
+import {Routes, Route, Navigate} from 'react-router-dom';
+
+import Home from './Food/Home';
+import Profile from './Food/Profile';
+import Search from './Food/Search';
+import Details from './Food/Details';
+import Login from './Food/Login';
+import Register from './Food/Register';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <HashRouter>
+      <div>
+        <Routes>
+          <Route path="/" element={<Navigate to ="/Home" />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/profile/*" element={<Profile />} />
+          <Route path="/search/*" element={<Search />} />
+          <Route path="/details/*" element={<Details />} />
+          <Route path="/login/*" element={<Login />} />
+          <Route path="/register/*" element={<Register />} />
+        </Routes>
+      </div>
+    </HashRouter>
   );
 }
 
---
+++ src/Food/Home/index.js
@@ -0,0 +1,19 @@
+import '../style.css'
+
+function Home() {
+    return (
+        <div>
+            <div className="food-title">
+                Food
+            </div>
+            <div className='food-subtitle'>
+                Trending Recipes
+                <br />
+                Feed
+
+            </div>
+        </div>
+    );
+}
+
+export default Home;
